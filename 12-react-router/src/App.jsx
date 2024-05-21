0f1be03a087e3bb2ba1fefb51b20d93283e4ed0a
sharma01ketan <sharma01ketan@gmail.com>
2024-05-21 22:46:31 +0530

    Initial commit

---
+++ 12-react-router/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-green-600 p-4'>React Router</h1>
+      <Header/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
